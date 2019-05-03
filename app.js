e922f0dba32fda151152b1de940602ce0d9c77a4
JMatthiasH <jamesmhow@gmail.com>
2019-05-03 14:37:44 -0400

    Initial Database Query in Express

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
 var app = express();
+var sqlite3 = require('sqlite3').verbose();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +18,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+let db = new sqlite3.Database('racket.sqlite3', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.log(err.message);
+    }
+    console.log('Connected to the Racket Database.');
+});
+db.serialize(() => {
+    db.each(`SELECT CompanyID as id, CompanyName as name FROM employers`, (err, row) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log(row.id + "\t" + row.name);
+    });
+});
+db.close((err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Close the Database Connection.');
+});
+// models.sequelize.sync().then(function() {
+//     console.log("DB Sync'd up");
+// });
 
 module.exports = app;
