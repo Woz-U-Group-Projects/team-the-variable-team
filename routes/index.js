ef6cdb68eede639286628ff15524643fe65ff2a8
JMatthiasH <jamesmhow@gmail.com>
2019-05-04 18:49:57 -0400

    working on DB routin

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var sqlite = require('sqlite3').verbose();
+
+const db = new sqlite.Database('./racket.sqlite', err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Yay! You are connected to the database!');
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
