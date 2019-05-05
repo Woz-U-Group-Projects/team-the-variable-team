37d5936ba404c984d9f396806f144f814db3f9c9
JMatthiasH <jamesmhow@gmail.com>
2019-05-04 23:13:39 -0400

    DB Syncd Up

--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-import { readdirSync } from 'fs';
-import { basename as _basename, join } from 'path';
-import Sequelize from 'sequelize';
-const basename = _basename(__filename);
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
@@ -15,12 +15,13 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-readdirSync(__dirname)
+fs
+  .readdirSync(__dirname)
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = sequelize['import'](join(__dirname, file));
+    const model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
@@ -33,4 +34,4 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-export default db;
+module.exports = db;
