1737512b186f07500ebd0260feb2eb581765a950
JMatthiasH <jamesmhow@gmail.com>
2019-05-12 19:18:46 -0400

    Updated routes/users.js file with working employer routes

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const sqlite = require('sqlite3').verbose();
 var models = require('../models');
+var Emp_Users = require('../models/Emp_Users');
 
 const db = new sqlite.Database('./racket.sqlite3', err => {
   if (err) {
@@ -10,48 +11,56 @@ const db = new sqlite.Database('./racket.sqlite3', err => {
   console.log('You are connnected to Racket DB');
 });
 
-/* GET home page. */
-
-
+/* Employer Routes */ 
 router.get('/employers', function (req, res, next) {
-  models.Emp_Users.findAll({}).then(employersAsPlainObject => {
-    const mappedEmployers = employersAsPlainObject.map(
-      sequelizeModelForEmployers => ({
-        ID: sequelizeModelForEmployers.ID,
-        CompanyName: sequelizeModelForEmployers.CompanyName,
-        CompanyWeb: sequelizeModelForEmployers.CompanyWeb,
-        CompanyEmail: sequelizeModelForEmployers.CompanyEmail,
-        CompanyContact: sequelizeModelForEmployers.CompanyContact,
-        CompContactNum: sequelizeModelForEmployers.CompContactNum,
-        Username: sequelizeModelForEmployers.Username,
-        Password: sequelizeModelForEmployers.Password
-      })
-    );
-    res.send(JSON.stringify(mappedEmployers));
+  models.Emp_Users.findAll({}).then(employers => {
+    res.send(JSON.stringify(employers));
   });
 });
 
-// router.get('/employers/:id', (req, res) => {
-//   let employerId = parseInt(req.params.ID);
-//   models.Emp_Users.findAll({
-//     where: {
-//       ID: 'ID'
-//     }
-//   }).then(employersAsPlainObject => {
-//     const mappedEmployers = employersAsPlainObject.map(
-//       sequelizeModelForEmployers => ({
-//         ID: sequelizeModelForEmployers.ID,
-//         CompanyName: sequelizeModelForEmployers.CompanyName,
-//         CompanyWeb: sequelizeModelForEmployers.CompanyWeb,
-//         CompanyEmail: sequelizeModelForEmployers.CompanyEmail,
-//         CompanyContact: sequelizeModelForEmployers.CompanyContact,
-//         CompContactNum: sequelizeModelForEmployers.CompContactNum,
-//         Username: sequelizeModelForEmployers.Username,
-//         Password: sequelizeModelForEmployers.Password
-//       })
-//     );
-//     res.send(JSON.stringify(mappedEmployers));
-//   });
+/* Route for Employer Profile by ID */
+router.get('/employers/:id', function(req, res) {
+  let employerId = parseInt(req.params.id);
+  models.Emp_Users.findOne({
+    where: {
+      ID: employerId
+    }
+  }).then(employer =>
+  res.send(JSON.stringify(employer)));
+});
+
+/* Create Employer Profile */
+// router.post('/', function(req, res, next) {
+//   console.log(req.body);
+//   EmpSignUp.create(
+//       {
+//           sku: req.body.sku,
+//           asin: req.body.asin,
+//           department: req.body.department,
+//           type: req.body.type,
+//           brand: req.body.brand,
+//           model: req.body.model,
+//           size: req.body.size,
+//           width: req.body.width,
+//           color: req.body.color,
+//           shipWeight: req.body.shipWeight,
+//           shipWidth: req.body.shipWidth,
+//           shipHeight: req.body.shipHeight,
+//           shipDepth: req.body.shipDepth,
+//           priceList: req.body.priceList,
+//           priceRetail: req.body.priceRetail,
+//           priceSavings: req.body.priceSavings,
+//           percentSavings: req.body.percentSavings,
+//           priceWholesale: req.body.priceWholesale
+//       },
+//       function(err, result) {
+//           if (err) {
+//               console.log(err);
+//           } else {
+//               res.json("Profile successfully created");
+//           }
+//       }
+//   );
 // });
 
 module.exports = router;
