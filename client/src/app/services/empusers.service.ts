830d4e4d215da58f2927f362758102bd6aca1a31
Caroline Macarius <carolinemacarius@gmail.com>
2019-05-14 21:36:29 -0700

    updating admin layout, [and empisers.service, stdusers.service were missing semicolon] login branch 5/14/2019

--- client/src/app/services/empusers.service.ts
+++ client/src/app/services/empusers.service.ts
@@ -5,12 +5,13 @@ import { HttpClient } from '@angular/common/http';
     providedIn: 'root'
 })
 export class EmpUsersService {
+    // tslint:disable-next-line:no-inferrable-types
     EmpURL: string = 'http://localhost:4001/users/employers';
     constructor(private http: HttpClient) { }
     getEmployers() {
-        return this.http.get(this.EmpURL)
+        return this.http.get(this.EmpURL);
     }
     getEmployer(id) {
-        return this.http.get(`${this.EmpURL}/${id}`)
+        return this.http.get(`${this.EmpURL}/${id}`);
     }
 }
--- client/src/app/services/stdusers.service.ts
+++ client/src/app/services/stdusers.service.ts
@@ -5,12 +5,13 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class StdUsersService {
+  // tslint:disable-next-line:no-inferrable-types
   StdURL: string = 'http://localhost:4001/users/students';
   constructor(private http: HttpClient) { }
   getStudents() {
-    return this.http.get(this.StdURL)
+    return this.http.get(this.StdURL);
   }
   getStudent(id) {
-    return this.http.get(`${this.StdURL}/${id}`)
+    return this.http.get(`${this.StdURL}/${id}`);
   }
 }
