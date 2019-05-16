050e101c3669434372162f509d59d13c8caafd24
andy avelar <andyavelar18@yahoo.com>
2019-05-16 15:48:10 -0700

    Angular-Model Files for Job Posts

---
+++ client/src/app/angular-models/Emp_JobPosts.ts
@@ -0,0 +1,10 @@
+export class Emp_JobPost {
+    Jobid: number;
+    JobName: string;
+    jobLocation: string;
+    jobWebsite: string;
+    jobContactNum: string;
+    jobDescription: string;
+    jobPostedDate: Date;
+    JobCreatedById: number
+}
---
+++ client/src/app/angular-models/Std_jobPost.ts
@@ -0,0 +1,11 @@
+export class Std_JobPosts {
+    Jobid: number;
+    jobName: string;
+    jobLocation: string;
+    jobWebsite: string;
+    jobContactNum: string;
+    jobEmail: string;
+    jobDescription: string;
+    jobPostedDate: Date;
+    JobCreatedById: number
+}
